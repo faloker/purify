fa0a4b9337e353d48228f181fc37c7136b5dd20d
faloker <faloker@tuta.io>
2020-02-02 19:08:50 +0200

    Add dialog to post comments

---
+++ web/src/components/dialogs/CommentDialog.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    max-width="650"
+    @click:outside="$emit('update:dialog', false)"
+  >
+    <v-card>
+      <v-container style="max-width: 600px;">
+        <v-timeline
+          dense
+          clipped
+        >
+          <v-timeline-item
+            class="mb-7"
+            large
+          >
+            <template v-slot:icon>
+              <v-avatar>
+                <img :src="currentUser.image">
+              </v-avatar>
+            </template>
+            <v-text-field
+              v-model="input"
+              hide-details
+              flat
+              label="Leave a comment..."
+              solo
+              @keydown.enter="postComment"
+            >
+              <template v-slot:append>
+                <v-btn
+                  :disabled="!input"
+                  outlined
+                  color="primary"
+                  @click="postComment"
+                >
+                  Post
+                </v-btn>
+              </template>
+            </v-text-field>
+          </v-timeline-item>
+
+          <v-slide-x-transition group>
+            <v-timeline-item
+              v-for="event in timeline"
+              :key="event.date"
+              class="mb-4"
+              small
+            >
+              <template v-slot:icon>
+                <v-avatar v-if="event.author.username === 'purify'">
+                  <img src="@/assets/purify-logo.png">
+                </v-avatar>
+                <v-avatar v-else>
+                  <img :src="event.author.image">
+                </v-avatar>
+              </template>
+              <v-row justify="space-between">
+                <v-col
+                  cols="7"
+                  v-text="event.text"
+                />
+                <v-col
+                  class="text-right"
+                  cols="5"
+                  v-text="new Date(event.date).toDateString()"
+                />
+              </v-row>
+            </v-timeline-item>
+          </v-slide-x-transition>
+        </v-timeline>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+import {
+  POST_COMMENT, ISSUES_FETCH,
+} from '@/store/actions';
+
+export default {
+  name: 'CommentDialog',
+  props: {
+    issue: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+    dialog: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      commentDialog: false,
+      input: null,
+    };
+  },
+  computed: {
+    ...mapGetters(['currentUser']),
+    timeline() {
+      return this.issue.comments ? this.issue.comments.slice().reverse() : [];
+    },
+  },
+  methods: {
+    postComment() {
+      if (this.input) {
+        const comment = { author: this.currentUser._id, text: this.input, date: new Date() };
+
+
+        this.$store.dispatch(POST_COMMENT, { id: this.issue._id, comment }).then((doc) => {
+          this.issue.comments.push(doc);
+          // this.events.push(doc);
+          this.input = null;
+          this.$store.dispatch(ISSUES_FETCH, this.$route.params.slug);
+        });
+      }
+    },
+  },
+};
+</script>
