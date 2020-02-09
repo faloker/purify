89c80e9c8436da61db7096b2a1571641e4c08d27
faloker <faloker@tuta.io>
2020-02-09 17:26:38 +0200

    Add func to delete/edit a unit

--- api/src/controllers/unitsController.js
+++ api/src/controllers/unitsController.js
@@ -6,7 +6,7 @@ import Project from '../models/Project';
 import Report from '../models/Report';
 import Issue from '../models/Issue';
 
-const createUnit = async (req, reply) => {
+export const createUnit = async (req, reply) => {
   const { body } = req;
 
   const project = await Project.findOne({ slug: body.project });
@@ -17,7 +17,7 @@ const createUnit = async (req, reply) => {
   reply.code(201).send(doc);
 };
 
-const getUnitsByProjectSlug = async (req, reply) => {
+export const getUnitsByProjectSlug = async (req, reply) => {
   const project = await Project.findOne({ slug: req.query.project });
 
   if (project) {
@@ -25,8 +25,13 @@ const getUnitsByProjectSlug = async (req, reply) => {
 
     for (const unit of units) {
       const numOfReports = await Report.countDocuments({ unit: { $eq: unit } });
-      const numberOfClosedTickets = await Issue.countDocuments({ unit: { $eq: unit }, is_closed: true });
-      const numberOfAllTickets = await Issue.countDocuments({ unit: { $eq: unit } });
+      const numberOfClosedTickets = await Issue.countDocuments({
+        unit: { $eq: unit },
+        is_closed: true,
+      });
+      const numberOfAllTickets = await Issue.countDocuments({
+        unit: { $eq: unit },
+      });
 
       unit._doc.reports = numOfReports;
       unit._doc.closed_tickets_len = numberOfClosedTickets;
@@ -39,10 +44,24 @@ const getUnitsByProjectSlug = async (req, reply) => {
   }
 };
 
-const fetchUnits = async () => Unit.find();
+export const editUnit = async (req, reply) => {
+  const doc = await Unit.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: req.body.change,
+    }
+  );
+  reply.send(doc);
+};
 
-export default {
-  createUnit,
-  getUnitsByProjectSlug,
-  fetchUnits,
+export const deleteUnit = async (req, reply) => {
+  await Report.deleteMany({ unit: req.params.id });
+
+  await Issue.deleteMany({ unit: req.params.id });
+
+  const stat = await Unit.deleteOne({ _id: req.params.id });
+
+  reply.send(stat);
 };
