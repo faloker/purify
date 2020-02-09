432adf35cf0aa1fabab60d49201b45e335bd645c
faloker <faloker@tuta.io>
2020-02-09 17:09:45 +0200

    Add rally preset

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,24 +1,26 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  preset,
   theme: {
     options: {
       customProperties: true,
     },
     themes: {
-      light: {
-        primary: '#43a047',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
+      dark: {
+        primary: '#1EB980',
+        secondary: '#045D56',
+        tertiary: '#FF6859',
+        quaternary: '#FFCF44',
+        quinary: '#B15DFF',
+        senary: '#72DEFF',
       },
     },
+    dark: true,
   },
   icons: {
     iconfont: 'mdi',
