d6ae73fe3858797ff17da9b8d825b296dd6b09ef
faloker <faloker@tuta.io>
2020-02-09 17:00:28 +0200

    Add edit/delete dialogs

--- web/src/views/Projects.vue
+++ web/src/views/Projects.vue
@@ -1,127 +1,106 @@
 <template>
-  <v-card
-    flat
-    height="100%"
-  >
-    <v-container>
-      <v-row>
-        <v-spacer />
-        <v-col>
-          <v-text-field
-            id="search"
-            ref="search"
-            v-model="search"
-            clearable
-            dense
-            outlined
-            @keydown.esc="onEsc"
-          >
-            <v-template slot="label">
-              <v-icon style="vertical-align: middle">
-                search
-              </v-icon>
-              Search for project
-            </v-template>
-          </v-text-field>
-        </v-col>
-        <v-col>
-          <v-btn
-            color="primary"
-            text
-            class=""
-            @click="dialog = true"
-          >
-            <v-icon>mdi-pencil</v-icon>
-            Create project
-          </v-btn>
-          <v-dialog
-            v-model="dialog"
-            max-width="500px"
-          >
-            <v-card>
-              <v-card-title>
-                <span class="mb-3 title">New project</span>
-              </v-card-title>
-              <v-spacer />
-              <v-card-text>
-                <v-layout wrap>
-                  <v-flex xs12>
-                    <v-text-field
-                      v-model="projectTitle"
-                      label="Project title"
-                      outlined
-                      dense
-                      clearable
-                      required
-                    />
-                  </v-flex>
-                  <v-flex xs12>
-                    <v-text-field
-                      v-model="projectSubtitle"
-                      label="Project short description"
-                      clearable
-                      dense
-                      outlined
-                      hint="For example, a tech stack: django, react, e.t.c"
-                      required
-                    />
-                  </v-flex>
-                </v-layout>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  color="red darken-1"
-                  text
-                  @click="dialog = false"
-                >
-                  Close
-                </v-btn>
-                <v-btn
-                  color="green darken-1"
-                  :disabled="projectTitle.length < 3"
-                  text
-                  @click="createProject"
-                >
-                  Create
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-col>
-      </v-row>
-      <v-row
-        align="center"
-        justify="space-around"
-      >
-        <v-template
-          v-for="project in filtredItems"
-          :key="project._id"
+  <v-container>
+    <v-row>
+      <v-spacer />
+      <v-col>
+        <v-text-field
+          id="search"
+          ref="search"
+          v-model="search"
+          clearable
+          dense
+          outlined
+          @keydown.esc="onEsc"
         >
-          <v-col>
-            <v-skeleton-loader
-              :loading="loading"
-              transition="scale-transition"
-              type="card"
-              width="400"
-            >
-              <project-card
-                :project="project"
-              />
-            </v-skeleton-loader>
-          </v-col>
-        </v-template>
-      </v-row>
-    </v-container>
-  </v-card>
+          <template slot="label">
+            <v-icon style="vertical-align: middle">
+              search
+            </v-icon>Search for project
+          </template>
+        </v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn
+          color="primary"
+          text
+          @click="dialog = true"
+        >
+          <v-icon>mdi-pencil</v-icon>Create project
+        </v-btn>
+        <v-dialog v-model="dialog" max-width="400px">
+          <v-card>
+            <v-card-title>
+              <span class="title">New project</span>
+            </v-card-title>
+            <v-spacer />
+            <v-card-text>
+              <v-col>
+                <v-row>
+                  <v-text-field
+                    v-model="projectTitle"
+                    label="Project title"
+                    outlined
+                    dense
+                    clearable
+                    required
+                  />
+                </v-row>
+                <v-row>
+                  <v-text-field
+                    v-model="projectSubtitle"
+                    label="Project short description"
+                    clearable
+                    dense
+                    outlined
+                    hint="For example, a tech stack: django, react, e.t.c"
+                    required
+                  />
+                </v-row>
+              </v-col>
+            </v-card-text>
+            <v-divider light></v-divider>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="tertiary"
+                text
+                @click="dialog = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                color="primary"
+                :disabled="projectTitle.length < 3"
+                text
+                @click="createProject"
+              >
+                Create
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="space-around">
+      <template v-for="project in filtredItems">
+        <v-col :key="project._id">
+          <v-skeleton-loader
+            :loading="loading"
+            transition="scale-transition"
+            type="card"
+            width="400"
+          >
+            <project-card :project="project" />
+          </v-skeleton-loader>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 import ProjectCard from '@/components/ProjectCard.vue';
-import {
-  FETCH_PROJECTS,
-  CREATE_PROJECT,
-} from '@/store/actions';
+import { FETCH_PROJECTS, CREATE_PROJECT } from '@/store/actions';
 import { SET_ACTIVE_PAGE } from '@/store/mutations';
 
 export default {
@@ -140,25 +119,29 @@ export default {
   computed: {
     ...mapGetters(['projectsList']),
     filtredItems() {
-      return this.projectsList.filter(
-        (item) => _.toLower(item.title + item.subtitle).includes(_.toLower(this.search)),
-      );
+      return this.projectsList.filter(item => _.toLower(item.title + item.subtitle).includes(_.toLower(this.search)));
     },
   },
   mounted() {
     this.$store.commit(SET_ACTIVE_PAGE, 'Projects');
-    this.$store.dispatch(FETCH_PROJECTS).then(() => { this.loading = false; });
+    this.$store.dispatch(FETCH_PROJECTS).then(() => {
+      this.loading = false;
+    });
 
-    document.onkeydown = (e) => {
+    document.onkeydown = e => {
       e = e || window.event;
       if (
         e.keyCode === 191 // Forward Slash '/'
-          && e.target !== this.$refs.search.$refs.input
+        && e.target !== this.$refs.search.$refs.input
       ) {
         e.preventDefault();
         this.$refs.search.focus();
-      } else if (e.keyCode === 67
-          && e.target !== this.$refs.search.$refs.input) { this.dialog = true; }
+      } else if (
+        e.keyCode === 67
+        && e.target !== this.$refs.search.$refs.input
+      ) {
+        this.dialog = true;
+      }
     };
   },
   methods: {
