c4601cc2b8cc92bfc68ff74a9d8af040401b6bd5
faloker <faloker@tuta.io>
2020-02-09 17:08:23 +0200

    Change appearance

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -1,42 +1,47 @@
 <template>
   <div>
     <v-navigation-drawer
-      v-model="drawer"
       app
-      temporary
+      permanent
+      dark
+      expand-on-hover
     >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img :src="currentUser.image" />
-        </v-list-item-avatar>
+      <!-- <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="@/assets/logo_trans.png" />
+      </v-list-item-avatar>-->
 
-        <v-list-item-content>
-          <v-list-item-title>{{ currentUser.username }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider />
-      <v-list dense>
-        <v-list-item
-          active-class="primary--text"
-          :to="{name: 'Dashboard'}"
-        >
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+      <!-- <v-list-item-content>
+            <v-list-item-title>{{ currentUser.username }}</v-list-item-title>
           </v-list-item-content>
-        </v-list-item>
-        <v-list-item
-          active-class="primary--text"
-          :to="{name: 'Projects'}"
-        >
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="tertiary">
+                mdi-logout
+              </v-icon>
+            </v-btn>
+      </v-list-item-action>-->
+      <!-- </v-list-item>
+      </v-list>-->
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item active-class="primary--text" :to="{ name: 'Projects' }">
           <v-list-item-action>
             <v-icon>mdi-apps</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Projects</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item active-class="primary--text" :to="{ name: 'Dashboard' }">
+          <v-list-item-action>
+            <v-icon>mdi-chart-bar</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <!-- <v-list-item
@@ -49,27 +54,35 @@
           <v-list-item-content>
             <v-list-item-title>Settings</v-list-item-title>
           </v-list-item-content>
-        </v-list-item> -->
+        </v-list-item>-->
       </v-list>
     </v-navigation-drawer>
 
     <v-app-bar
       app
-      color="primary"
-      dark
       flat
-      dense
       hide-on-scroll
+      color="#33333d"
+      dense
     >
-      <v-btn
-        icon
-        @click="drawer = !drawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-badge
+          color="primary"
+          content="6"
+          overlap
+          bottom
+          left
+        >
+          <v-icon>mdi-bell</v-icon>
+        </v-badge>
       </v-btn>
-      <v-toolbar-title>
-        <span id="active-page-name" class="font-weight-bold">{{ activePage }}</span>
-      </v-toolbar-title>
+      <v-avatar class="mx-2" size="36">
+        <img :src="currentUser.image" alt="ava" />
+      </v-avatar>
+      <!-- <v-toolbar-title>
+        <span id="active-page-name" class="ml-4 font-weight-bold">{{ activePage }}</span>
+      </v-toolbar-title>-->
     </v-app-bar>
   </div>
 </template>
