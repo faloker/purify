b25257ccc71edeb7a2f69381ad365c07dc6f9eb3
faloker <faloker@tuta.io>
2020-02-15 21:02:11 +0200

    Refactor

--- web/src/common/utils.servive.js
+++ web/src/common/utils.servive.js
@@ -1,18 +1,10 @@
-import _ from 'lodash';
-
-const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-const matchPattern = (source, template) => {
-  const resolve = match => _.get(source, match.replace(/[{}]/g, ''));
-  return template.replace(/(\{.[^}]*\})/g, resolve);
+// eslint-disable-next-line import/prefer-default-export
+export const matchPattern = (fields, template) => {
+  let result = template;
+  for (const field of Object.keys(fields)) {
+    if (template.includes(field)) {
+      result = result.replace(field, fields[field]);
+    }
+  }
+  return result;
 };
-
-const dateDiffInDays = (a, b) => {
-  a = new Date(a);
-  b = new Date(b);
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-};
-
-export { matchPattern, dateDiffInDays };
