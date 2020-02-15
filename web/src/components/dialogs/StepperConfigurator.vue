1fcbe48373907f51571b5f3aea224e83273ccc10
faloker <faloker@tuta.io>
2020-02-15 21:02:49 +0200

    Removed a card component from steps

--- web/src/components/dialogs/StepperConfigurator.vue
+++ web/src/components/dialogs/StepperConfigurator.vue
@@ -10,294 +10,283 @@
         non-linear
         vertical
       >
-        <v-stepper-step :complete="e6 > 1" step="1">
+        <v-stepper-step
+          :complete="e6 > 1"
+          step="1"
+        >
           Select the array with the findings. You can view an example finding object in an array.
         </v-stepper-step>
         <v-stepper-content step="1">
-          <v-card class="fill-height" flat>
-            <v-btn
-              class="mx-2"
-              outlined
-              @click="getContent(reportId)"
-            >
-              View report
-            </v-btn>
-            <v-btn
-              class="mx-2"
-              :disabled="!path_to_issues"
-              outlined
-              color="primary"
-              @click="e6 = 2"
-            >
-              Next
-            </v-btn>
-            <v-card-text>
-              You have chosen the following:
-              <b class="ml-3">{{ path_to_issues }}</b>
-            </v-card-text>
-          </v-card>
+          <v-btn
+            class="mx-2"
+            outlined
+            @click="getContent(reportId)"
+          >
+            View report
+          </v-btn>
+          <v-btn
+            class="mx-2"
+            :disabled="!path_to_issues"
+            outlined
+            color="primary"
+            @click="e6 = 2"
+          >
+            Next
+          </v-btn>
+          <v-card-text>
+            You have chosen the following:
+            <b class="ml-3">{{ path_to_issues }}</b>
+          </v-card-text>
         </v-stepper-content>
-        <v-stepper-step :complete="e6 > 2" step="2">
+        <v-stepper-step
+          :complete="e6 > 2"
+          step="2"
+        >
           Which fields will be in issue title and subtitle?
         </v-stepper-step>
         <v-stepper-content step="2">
-          <v-card class="fill-height" flat>
-            <vue-json-pretty
-              v-model="title_fields"
-              :deep="2"
-              :path="'issue'"
-              :selectable-type="'multiple'"
-              :path-selectable="(path, data) => path !== 'issue'"
-              :data="exampleIssue"
-              :show-select-controller="true"
-              :highlight-selected-node="false"
-            />
-            <v-btn
-              class="mx-2 mt-3"
-              outlined
-              :disabled="!title_fields.length"
-              color="primary"
-              @click="e6 = 3"
-            >
-              Next
-            </v-btn>
-            <v-btn
-              class="mx-2 mt-3"
-              outlined
-              @click="e6 = 1"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <vue-json-pretty
+            v-model="title_fields"
+            :deep="2"
+            :path="'issue'"
+            :selectable-type="'multiple'"
+            :path-selectable="(path, data) => path !== 'issue'"
+            :data="exampleIssue"
+            :show-select-controller="true"
+            :highlight-selected-node="false"
+          />
+          <v-btn
+            class="mx-2 mt-3"
+            outlined
+            :disabled="!title_fields.length"
+            color="primary"
+            @click="e6 = 3"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mx-2 mt-3"
+            outlined
+            @click="e6 = 1"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
         <v-stepper-step :complete="e6 > 3" step="3">
           Add patterns to display title and subtitle
         </v-stepper-step>
         <v-stepper-content step="3">
-          <v-card class="fill-height" flat>
-            <v-combobox
-              v-model="title_pattern"
-              label="Pattern for a title"
-              hint="For example, [{issue.severity}] -- {issue.title}"
-              persistent-hint
-              :items="title_fields.map(i => i.replace('issue.', ''))"
-            />
-            <v-combobox
-              v-model="subtitle_pattern"
-              class="pt-4"
-              label="Pattern for a subtitle"
-              hint="For example, {issue.CVSS} | {issue.short_description}"
-              persistent-hint
-              :items="title_fields.map(i => i.replace('issue.', ''))"
-            />
-            <v-btn
-              :disabled="!title_fields.length"
-              color="primary"
-              class="mx-2 mt-3"
-              outlined
-              @click="e6 = 4"
-            >
-              Next
-            </v-btn>
-            <v-btn
-              class="mx-2 mt-3"
-              outlined
-              @click="e6 = 2"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <v-combobox
+            v-model="title_pattern"
+            label="Pattern for a title"
+            hint="For example, [issue.severity] -- issue.title"
+            persistent-hint
+            :items="title_fields.map(i => i.replace('issue.', ''))"
+          />
+          <v-combobox
+            v-model="subtitle_pattern"
+            class="pt-4"
+            label="Pattern for a subtitle"
+            hint="For example, issue.CVSS | issue.short_description"
+            persistent-hint
+            :items="title_fields.map(i => i.replace('issue.', ''))"
+          />
+          <v-btn
+            :disabled="!title_fields.length"
+            color="primary"
+            class="mx-2 mt-3"
+            outlined
+            @click="e6 = 4"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mx-2 mt-3"
+            outlined
+            @click="e6 = 2"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
         <v-stepper-step :complete="e6 > 4" step="4">
           Which fields will be in issue body?
         </v-stepper-step>
         <v-stepper-content step="4">
-          <v-card class="fill-height" flat>
-            <vue-json-pretty
-              v-model="body_fields"
-              :deep="2"
-              :path="'issue'"
-              :selectable-type="'multiple'"
-              :path-selectable="(path, data) => path !== 'issue'"
-              :data="exampleIssue"
-              :show-select-controller="true"
-              :highlight-selected-node="false"
-            />
-            <v-btn
-              :disabled="!body_fields.length"
-              color="primary"
-              class="mx-2 mt-3"
-              outlined
-              @click="e6 = 5"
-            >
-              Next
-            </v-btn>
-            <v-btn
-              class="mx-2 mt-3"
-              outlined
-              @click="e6 = 3"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <vue-json-pretty
+            v-model="body_fields"
+            :deep="2"
+            :path="'issue'"
+            :selectable-type="'multiple'"
+            :path-selectable="(path, data) => path !== 'issue'"
+            :data="exampleIssue"
+            :show-select-controller="true"
+            :highlight-selected-node="false"
+          />
+          <v-btn
+            :disabled="!body_fields.length"
+            color="primary"
+            class="mx-2 mt-3"
+            outlined
+            @click="e6 = 5"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mx-2 mt-3"
+            outlined
+            @click="e6 = 3"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
         <v-stepper-step :complete="e6 > 5" step="5">
           Select types of fields in issue body
         </v-stepper-step>
         <v-stepper-content step="5">
-          <v-card class="fill-height" flat>
-            <template v-for="item in body_fields">
-              <div :key="item" class="my-2">
-                <span>
-                  {{ item }}
-                </span>
-                <v-btn-toggle
-                  v-model="type_body_fields[item]"
-                  color="primary"
-                  group
-                >
-                  <v-btn small value="text">
-                    Text
-                  </v-btn>
-
-                  <v-btn small value="html">
-                    Html
-                  </v-btn>
-
-                  <v-btn small value="base64">
-                    Base64
-                  </v-btn>
-                </v-btn-toggle>
-              </div>
-            </template>
-            <v-card-actions>
-              <v-btn
-                :disabled="Object.keys(type_body_fields).length !== body_fields.length"
+          <template v-for="item in body_fields">
+            <div :key="item" class="my-2">
+              <span class="subtitle-1">{{ item }}</span>
+              <v-btn-toggle
+                v-model="type_body_fields[item]"
                 color="primary"
-                class="mr-2 mt-3"
-                outlined
-                @click="e6 = 6"
+                group
               >
-                Next
-              </v-btn>
-              <v-btn
-                class="mr-2 mt-3"
-                outlined
-                @click="e6 = 4"
-              >
-                Go back
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+                <v-btn small value="text">
+                  Text
+                </v-btn>
+
+                <v-btn small value="html">
+                  Html
+                </v-btn>
+
+                <v-btn small value="base64">
+                  Base64
+                </v-btn>
+              </v-btn-toggle>
+            </div>
+          </template>
+          <v-btn
+            :disabled="Object.keys(type_body_fields).length !== body_fields.length"
+            color="primary"
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 6"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 4"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
-        <v-stepper-step :complete="e6 > 6" step="6">
+        <v-stepper-step
+          :complete="e6 > 6"
+          step="6"
+        >
           Select field(s) that will be used to detect duplicates
         </v-stepper-step>
         <v-stepper-content step="6">
-          <v-card class="fill-height" flat>
-            <vue-json-pretty
-              v-model="compare_fields"
-              :deep="2"
-              :path="'issue'"
-              :selectable-type="'multiple'"
-              :path-selectable="(path, data) => path !== 'issue'"
-              :data="exampleIssue"
-              :show-select-controller="true"
-              :highlight-selected-node="false"
-            />
-            <v-btn
-              :disabled="!compare_fields.length"
-              color="primary"
-              class="mx-2"
-              outlined
-              @click="e6 = 7"
-            >
-              Next
-            </v-btn>
-            <v-btn
-              class="mx-2"
-              outlined
-              @click="e6 = 5"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <vue-json-pretty
+            v-model="compare_fields"
+            :deep="2"
+            :path="'issue'"
+            :selectable-type="'multiple'"
+            :path-selectable="(path, data) => path !== 'issue'"
+            :data="exampleIssue"
+            :show-select-controller="true"
+            :highlight-selected-node="false"
+          />
+          <v-btn
+            :disabled="!compare_fields.length"
+            color="primary"
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 7"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 5"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
         <v-stepper-step :complete="e6 > 7" step="7">
           Select field(s) that will be merged if the issues look the same (based on duplication
           score)
         </v-stepper-step>
         <v-stepper-content step="7">
-          <v-card class="fill-height" flat>
-            <vue-json-pretty
-              v-model="merge_fields"
-              :deep="2"
-              :path="'issue'"
-              :selectable-type="'multiple'"
-              :path-selectable="(path, data) => path !== 'issue'"
-              :data="exampleIssue"
-              :show-select-controller="true"
-              :highlight-selected-node="false"
-            />
-            <v-btn
-              :disabled="!merge_fields.length"
-              color="primary"
-              class="mx-2"
-              outlined
-              @click="e6 = 8"
-            >
-              Next
-            </v-btn>
-            <v-btn
-              class="mx-2"
-              outlined
-              @click="e6 = 6"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <vue-json-pretty
+            v-model="merge_fields"
+            :deep="2"
+            :path="'issue'"
+            :selectable-type="'multiple'"
+            :path-selectable="(path, data) => path !== 'issue'"
+            :data="exampleIssue"
+            :show-select-controller="true"
+            :highlight-selected-node="false"
+          />
+          <v-btn
+            :disabled="!merge_fields.length"
+            color="primary"
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 8"
+          >
+            Next
+          </v-btn>
+          <v-btn
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 6"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
         <v-stepper-step step="8">
           Give a name for this template
         </v-stepper-step>
         <v-stepper-content step="8">
-          <v-card class="fill-height" flat>
-            <v-text-field
-              v-model="name"
-              class="tname"
-              :rules="[rules.min]"
-              label="Template Name"
-              prepend-icon="short_text"
-              clearable
-              required
-            />
-            <v-combobox
-              v-model="tags"
-              class="tname"
-              label="Add some tags"
-              prepend-icon="mdi-tag-multiple"
-              chips
-              small-chips
-              multiple
-            />
-            <v-btn
-              color="primary"
-              class="mx-2"
-              outlined
-              :loading="loading"
-              :disabled="loading"
-              @click="saveTemplate()"
-            >
-              Save
-            </v-btn>
-            <v-btn
-              class="mx-2"
-              outlined
-              @click="e6 = 7"
-            >
-              Go back
-            </v-btn>
-          </v-card>
+          <v-text-field
+            v-model="name"
+            class="tname"
+            :rules="[rules.min]"
+            label="Template Name"
+            prepend-icon="short_text"
+            clearable
+            required
+          />
+          <v-combobox
+            v-model="tags"
+            class="tname"
+            label="Add some tags"
+            prepend-icon="mdi-tag-multiple"
+            chips
+            small-chips
+            multiple
+          />
+          <v-btn
+            color="primary"
+            class="mr-2 mt-3"
+            outlined
+            :loading="loading"
+            :disabled="loading"
+            @click="saveTemplate()"
+          >
+            Save
+          </v-btn>
+          <v-btn
+            class="mr-2 mt-3"
+            outlined
+            @click="e6 = 7"
+          >
+            Go back
+          </v-btn>
         </v-stepper-content>
       </v-stepper>
     </v-dialog>
@@ -407,9 +396,9 @@ export default {
           subtitle_pattern: this.subtitle_pattern,
           tags: this.tags,
           body_fields: this.body_fields,
-          merge_fields: this.merge_fields.map((i) => i.replace('issue.', '')),
-          title_fields: this.title_fields.map((i) => i.replace('issue.', '')),
-          compare_fields: this.compare_fields.map((i) => i.replace('issue.', '')),
+          merge_fields: this.merge_fields.map(i => i.replace('issue.', '')),
+          title_fields: this.title_fields.map(i => i.replace('issue.', '')),
+          compare_fields: this.compare_fields.map(i => i.replace('issue.', '')),
         })
         .then(() => {
           this.loading = false;
