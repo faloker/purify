2cd2d6fc36d827fba2eb9526d544028784eb8edb
faloker <faloker@tuta.io>
2020-02-29 20:04:00 +0200

    Use cdn for production deployment

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,27 @@
-module.exports = {
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const config = {
   transpileDependencies: ['vuetify'],
+  productionSourceMap: false,
 };
+
+if (process.env.NODE_ENV === 'production') {
+  const configureWebpack = {
+    plugins: [
+      new MomentLocalesPlugin(),
+      // new BundleAnalyzerPlugin(),
+    ],
+    externals: {
+      'chart.js': 'Chart',
+      vue: 'Vue',
+      lodash: '_',
+      'vue-toasted': 'Toasted',
+      'vue-router': 'VueRouter',
+      'vue-simplemde': 'VueSimpleMDE',
+    },
+  };
+  config.configureWebpack = configureWebpack;
+}
+
+module.exports = config;
