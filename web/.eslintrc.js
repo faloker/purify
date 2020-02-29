c79c0555ebfc01a8b5277c7d8a041ff89c32c5ec
faloker <faloker@tuta.io>
2020-02-29 19:59:28 +0200

    Remove globals

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  "globals": {
-    "_": "readonly",
-  },
-  extends: ["plugin:vue/recommended", "@vue/airbnb"],
-  plugins: ["vuetify"],
+  extends: ['plugin:vue/recommended', '@vue/airbnb'],
+  plugins: ['vuetify'],
   rules: {
-    "arrow-parens": ["error", "as-needed"],
-    "vue/html-self-closing": "off",
-    "vue/html-closing-bracket-spacing": "off",
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": 2,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
-      }
+    'arrow-parens': ['error', 'as-needed'],
+    'vue/html-self-closing': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 2,
+      multiline: {
+        max: 1,
+        allowFirstLine: false,
+      },
     }],
-    "no-underscore-dangle": "off",
-    "no-restricted-syntax": "off",
-    "vuetify/no-deprecated-classes": "error",
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-underscore-dangle': 'off',
+    'no-restricted-syntax': 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
 };
