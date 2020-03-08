c743f3111da143acbb3904f6f06238a4a78d78dd
faloker <faloker@tuta.io>
2020-03-08 23:32:27 +0200

    Update config files

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  root: true,
   env: {
     node: true,
-    es6: true
+    jest: true,
   },
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true }],
-    "no-await-in-loop": "off",
-    "func-names": "off",
-    "no-restricted-syntax": "off",
-    "no-underscore-dangle": "off",
-  }
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
