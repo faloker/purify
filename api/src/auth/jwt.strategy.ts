f94b505eafffa56e7fac95db49e0948b880929e6
faloker <faloker@tuta.io>
2020-03-18 17:16:49 +0200

    Update strategy to reflect changes in the auth mech

--- api/src/auth/jwt.strategy.ts
+++ api/src/auth/jwt.strategy.ts
@@ -1,19 +1,23 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
-import { jwtConstants } from './constants';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: any) {
-    return { id: payload.id };
+    if (payload.type === 'access_token') {
+      return { id: payload.id };
+    } else {
+      throw new UnauthorizedException();
+    }
   }
 }
