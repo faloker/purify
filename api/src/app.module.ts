e9c5da4c441872124efb1bf8e1c6fe8ffa4c2ae4
faloker <faloker@tuta.io>
2020-03-18 17:07:07 +0200

    Update module deps

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,36 +1,53 @@
-import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UnitsModule } from './units/units.module';
 import { IssuesModule } from './issues/issues.module';
 import { ReportsModule } from './reports/reports.module';
 import { TemplatesModule } from './templates/templates.module';
+import { TasksModule } from './tasks/tasks.module';
+import { JiraModule } from './jira/jira.module';
+import { SettingsModule } from './settings/settings.module';
+import { SmtpModule } from './smtp/smtp.module';
 
 @Module({
   imports: [
-    UsersModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      poolSize: 10,
-      connectTimeoutMS: 10000,
-      socketTimeoutMS: 45000,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        poolSize: 10,
+        connectTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+      }),
+      inject: [ConfigService],
     }),
+    ServeStaticModule.forRoot({
+      rootPath: '/home/node/app/static',
+      exclude: ['/api*'],
+    }),
+    ConfigModule.forRoot(),
     AuthModule,
+    UsersModule,
     ProjectsModule,
     UnitsModule,
     IssuesModule,
     ReportsModule,
     TemplatesModule,
+    TasksModule,
+    JiraModule,
+    SettingsModule,
+    SmtpModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
