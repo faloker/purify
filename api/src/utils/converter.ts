b35249381c454cd7a7c32457282a81bf0e576ddf
faloker <faloker@tuta.io>
2020-03-18 17:11:52 +0200

    Add xml converter

---
+++ api/src/utils/converter.ts
@@ -0,0 +1,23 @@
+import { xml2json } from 'xml-js';
+
+const removeAttribute = (value, parentElement) => {
+  try {
+    const keyNo = Object.keys(parentElement._parent).length;
+    const keyName = Object.keys(parentElement._parent)[keyNo - 1];
+    parentElement._parent[keyName] = value;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export function xmlToJson(data) {
+  JSON.parse(
+    xml2json(data, {
+      compact: true,
+      spaces: 4,
+      ignoreAttributes: true,
+      textFn: removeAttribute,
+      cdataFn: removeAttribute,
+    })
+  );
+};
