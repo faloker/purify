0d48e1cd213dd12d9416580803c28b378a6e61fd
faloker <faloker@tuta.io>
2020-03-28 16:34:40 +0200

    Use awk instead of grep

--- docker/nginx-entrypoint.sh
+++ docker/nginx-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-HOST=$(grep -m1 -Poe 'server_name \K[^;\s]+' /etc/nginx/nginx.conf)
-PORT=$(grep -m1 -Poe 'listen \K[^;\s]+' /etc/nginx/nginx.conf)
+HOST=$(awk '$1=="server_name"{sub(/;/,""); print $2; exit}' /etc/nginx/nginx.conf)
+PORT=$(awk '$1=="listen"{sub(/;/,""); print $2; exit}' /etc/nginx/nginx.conf)
 
 if grep -q ssl /etc/nginx/nginx.conf; then
     PROTOCOL=https
