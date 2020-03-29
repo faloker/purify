fbad01a0782745b53333a88597463e256d1df364
faloker <faloker@tuta.io>
2020-03-29 19:00:25 +0300

    Fix cypress config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,32 +2,4 @@ const mongoose = require('mongoose');
 
 module.exports = (on, config) => {
   on('task', require('@cypress/code-coverage/task'));
-
-  // on('task', {
-  //   'flush:db': async () => {
-  //     const url = 'mongodb://localhost:27017/test';
-
-  //     // const conn = mongoose.createConnection(url, {
-  //     //   useNewUrlParser: true,
-  //     //   useUnifiedTopology: true,
-  //     //   useFindAndModify: false,
-  //     //   useCreateIndex: true,
-  //     // });
-  //     // const url = 'mongodb://localhost:27017/purify';
-  //     await mongoose.connect(url)
-
-  //     await mongoose.connection.collection('users').remove();
-  //     // await conn.collection('projects').remove();
-  //     // await conn.collection('units').remove();
-  //     // await conn.collection('reports').remove();
-  //     // await conn.collection('templates').remove();
-  //     // await conn.collection('tickets').remove();
-  //     // await conn.collection('issues').remove();
-  //     // await conn.collection('comments').remove();
-  //     // await conn.collection('jirasettings').remove();
-  //     // await conn.collection('slacksettings').remove();
-
-  //     return null;
-  //   },
-  // });
 };
