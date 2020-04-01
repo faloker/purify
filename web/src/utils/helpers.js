d5e97a370d92b4b1ec6f2e39f6104bec775582fb
faloker <faloker@tuta.io>
2020-04-01 23:17:00 +0300

    Change the way to resolve complex paths

--- web/src/utils/helpers.js
+++ web/src/utils/helpers.js
@@ -1,13 +1,19 @@
 import { get, capitalize, startCase } from 'lodash';
 import { formatDistance } from 'date-fns';
 
-export const matchPattern = (fields, template) => {
-  let result = template;
-  for (const field of Object.keys(fields)) {
-    if (template.includes(field)) {
-      result = result.replace(field, fields[field]);
+export const matchPattern = (fields, pattern) => {
+  let result = pattern;
+  const matches = pattern
+    // eslint-disable-next-line no-useless-escape
+    .match(/[a-zA-Z0-9\.\[\]]+/gm);
+
+  for (let match of matches) {
+    if (match.startsWith('[') && match.endsWith(']')) {
+      match = match.replace(/^\[/, '').replace(/]$/, '');
     }
+    result = result.replace(match, get(fields, match));
   }
+
   return result;
 };
 
