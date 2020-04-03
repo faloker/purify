a8d90e21873d03daab2f92d8f97e72b29f189923
faloker <faloker@tuta.io>
2020-04-03 12:37:33 +0300

    Seperate markdown creation

--- web/src/utils/helpers.js
+++ web/src/utils/helpers.js
@@ -27,3 +27,62 @@ export const parseKey = key => {
 export const getValue = (fields, key) => get(fields, key);
 
 export const formatDate = date => `${formatDistance(new Date(date), new Date())} ago`;
+
+const isPrintable = obj => ['string', 'boolean', 'number'].includes(typeof obj);
+
+const decodeValue = str => atob(str);
+
+
+const parseField = (field, fieldValue) => {
+  let mkdwn = '';
+  const fieldKey = field.key || field;
+  const fieldType = field.type || 'text';
+
+  if (isPrintable(fieldValue)) {
+    mkdwn += `## ${parseKey(fieldKey)}\n`;
+
+    if (fieldType === 'base64') {
+      mkdwn += `${decodeValue(fieldValue)}\n\n`;
+    } else {
+      mkdwn += `${fieldValue}\n\n`;
+    }
+  } else if (Array.isArray(fieldValue)) {
+    if (isPrintable(fieldValue[0])) {
+      mkdwn += `## ${parseKey(fieldKey)}\n`;
+
+      for (const element of fieldValue) {
+        mkdwn += `* ${element}\n`;
+      }
+
+      mkdwn += '\n';
+    } else if (typeof fieldValue[0] === 'object') {
+      mkdwn += `## ${parseKey(fieldKey)}\n`;
+
+      for (const [index, element] of fieldValue.entries()) {
+        mkdwn += `<details><summary><b># ${index}</b></summary><p>\n\n`;
+
+        for (const key of Object.keys(element)) {
+          mkdwn += parseField(key, element[key]);
+        }
+
+        mkdwn += '</p></details>';
+      }
+    } else if (typeof fieldValue === 'object') {
+      for (const key of Object.keys(fieldValue)) {
+        mkdwn += parseField(key, fieldValue[key]);
+      }
+    }
+  }
+
+  return mkdwn;
+};
+
+export const prepareMarkdown = issue => {
+  let result = '';
+
+  for (const field of issue.template.body_fields) {
+    result += parseField(field, getValue(issue.fields, field.key));
+  }
+
+  return result;
+};
