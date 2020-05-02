279f86a35bd36e725fa90d336970a20b9b247375
faloker <faloker@tuta.io>
2020-05-02 17:04:58 +0300

    Add mini-profile menu popover

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -5,27 +5,6 @@
       absolute
       temporary
     >
-      <!-- <v-list>
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="@/assets/logo_trans.png" />
-      </v-list-item-avatar>-->
-
-      <!-- <v-list-item-content>
-            <v-list-item-title>{{ currentUser.username }}</v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn icon>
-              <v-icon color="tertiary">
-                mdi-logout
-              </v-icon>
-            </v-btn>
-      </v-list-item-action>-->
-      <!-- </v-list-item>
-      </v-list>-->
-
-      <v-divider></v-divider>
-
       <v-list nav dense>
         <v-list-item active-class="primary--text" :to="{ name: 'Projects' }">
           <v-list-item-action>
@@ -51,17 +30,6 @@
             <v-list-item-title>Templates</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <!-- <v-list-item
-          active-class="primary--text"
-          :to="{name: 'Settings'}"
-        >
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>-->
       </v-list>
     </v-navigation-drawer>
 
@@ -73,50 +41,94 @@
       dense
       dark
     >
-      <!-- <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Tab 1</v-tab>
-          <v-tab>Tab 2</v-tab>
-          <v-tab>Tab 3</v-tab>
-        </v-tabs>
-      </template>-->
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <b>Purify</b>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <!-- <v-btn icon>
-        <v-badge
-          color="primary"
-          content="6"
-          overlap
-          bottom
-          left
+      <div class="text-center">
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-width="200"
+          offset-x
+          transition="slide-y-transition"
         >
-          <v-icon>mdi-bell</v-icon>
-        </v-badge>
-      </v-btn>
-      <v-avatar class="mx-2" size="36">
-        <img :src="currentUser.image" alt="ava" />
-      </v-avatar>-->
-      <!-- <v-toolbar-title>
-        <span id="active-page-name" class="ml-4 font-weight-bold">{{ activePage }}</span>
-      </v-toolbar-title>-->
+          <template v-slot:activator="{ on }">
+            <v-btn icon>
+              <v-avatar size="40" v-on="on">
+                <img :src="currentUser.image" alt="ava" />
+              </v-avatar>
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-list>
+              <v-list-item>
+                <v-list-item-avatar>
+                  <img :src="currentUser.image" alt="ava" />
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ currentUser.username }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ currentUser.email }}</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-action>
+                  <v-btn icon @click="killSession">
+                    <v-icon>mdi-exit-to-app</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list>
+              <v-list-item-group v-model="item" color="primary">
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>mdi-tune</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>Account settings</v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>mdi-bell</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>Notifications</v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card>
+        </v-menu>
+      </div>
     </v-app-bar>
   </div>
 </template>
 <script>
 import { mapGetters } from 'vuex';
+import { LOGOUT } from '@/store/actions';
 
 export default {
   name: 'TheHeader',
   data() {
     return {
+      item: null,
+      menu: false,
       drawer: false,
     };
   },
   computed: {
     ...mapGetters(['activePage', 'currentUser']),
+  },
+  methods: {
+    killSession() {
+      this.$store.dispatch(LOGOUT).then(() => {
+        this.$toasted.global.api_success({ msg: 'Bye' });
+        this.$router.replace('welcome');
+      });
+    },
   },
 };
 </script>
