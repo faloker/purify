de8f0a355477fbd9625884034d1bb5f0411be33d
faloker <faloker@tuta.io>
2020-06-01 22:17:43 +0300

    Update getIssues handler

--- api/src/issues/dto/issues.dto.ts
+++ api/src/issues/dto/issues.dto.ts
@@ -1,19 +1,57 @@
 import {
-  IsEmail,
   IsNotEmpty,
   IsString,
-  MinLength,
   IsArray,
   ArrayNotEmpty,
   IsObject,
+  IsUUID,
+  IsOptional,
+  IsIn,
 } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetIssuesQueryDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The slug of a unit you want to get issues for.',
+  })
   @IsString()
   @IsNotEmpty()
   readonly unit: string;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: 'Filter by issue status.',
+  })
+  @IsString()
+  @IsIn(['true', 'false'])
+  @IsOptional()
+  readonly closed?: string;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: 'Filter by ticket.',
+  })
+  @IsString()
+  @IsIn(['true', 'false'])
+  @IsOptional()
+  readonly ticket?: string;
+
+  @ApiPropertyOptional({
+    description: 'Filter by risks. Provide a comma-separated list of risks.',
+  })
+  @IsOptional()
+  @IsString()
+  readonly risks?: string;
+
+  @ApiPropertyOptional({
+    type: Boolean,
+    description: 'Include addtional fields, such as comments and template.',
+    default: 'true',
+  })
+  @IsString()
+  @IsIn(['true', 'false'])
+  @IsOptional()
+  readonly verbose?: string;
 }
 
 export class UpdateIssuesBodyDto {
@@ -29,7 +67,7 @@ export class UpdateIssuesBodyDto {
 
 export class IdParamDto {
   @ApiProperty()
-  @IsString()
+  @IsUUID('4')
   @IsNotEmpty()
   readonly id: string;
 }
