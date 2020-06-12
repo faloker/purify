1abeaf927852417abdd74bd5e774dff5c16b37e3
faloker <faloker@tuta.io>
2020-06-12 23:48:37 +0300

    Change filter appearance

---
+++ web/src/components/FilterOption.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-item-group
+    v-model="selection"
+    multiple
+    @change="$emit('update:selection', selection)"
+  >
+    <v-sheet
+      class="px-2"
+      height="150"
+      width="200"
+    >
+      <v-container>
+        <v-row align="start">
+          <template v-if="!searchBar">
+            <strong class="text--secondary pl-1">{{ name }}</strong>
+            <v-spacer />
+            <!-- <v-btn
+              icon
+              @click="openSearch"
+            >
+              <v-icon small>
+                fa-search
+              </v-icon>
+            </v-btn>-->
+          </template>
+          <!-- <v-autocomplete
+            v-else
+            v-model="search"
+            multiple
+            :items="titles"
+            clearable
+            hide-no-data
+            dense
+            @click:clear="searchBar = false"
+          />-->
+        </v-row>
+        <v-row>
+          <v-sheet
+            height="100"
+            width="250"
+            style="overflow-y: scroll;"
+            tile
+          >
+            <template v-for="item in items">
+              <v-item
+                v-slot:default="{ active, toggle }"
+                :key="item.title"
+                :value="`${item.title}`"
+              >
+                <v-progress-linear
+                  :value="item.value"
+                  :color="active ? 'grey lighten-3' : 'grey lighten-3'"
+                  background-color="white"
+                  height="25"
+                  @click="toggle"
+                >
+                  <span v-if="!active" class="text--disabled pl-1 text-capitalize">{{ item.title }}</span>
+                  <strong v-else class="primary--text pl-1 text-capitalize">{{ item.title }}</strong>
+                  <v-spacer />
+                  <strong v-if="item.total && active" class="primary--text pr-1">{{ item.total }}</strong>
+                  <span
+                    v-else-if="item.total && !active"
+                    class="text--disabled pr-1"
+                  >{{ item.total }}</span>
+                </v-progress-linear>
+              </v-item>
+            </template>
+          </v-sheet>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-item-group>
+</template>
+<script>
+export default {
+  name: 'FilterOption',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    selection: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    // search: [],
+    // searchBar: false,
+  }),
+  // computed: {
+  // titles() {
+  //   return this.items.map((item) => item.title);
+  // },
+  // filteredItems() {
+  //   return this.search.length
+  //     ? this.items.filter((item) => this.search.includes(item.title))
+  //     : this.items;
+  // },
+  // },
+  // methods: {
+  //   openSearch() {
+  //     this.searchBar = true;
+  //   },
+  // },
+};
+</script>
--- web/src/components/IssueFilter.vue
+++ web/src/components/IssueFilter.vue
@@ -1,162 +1,150 @@
 <template>
-  <v-card outlined color="#fafafa">
-    <v-container fluid>
-      <v-row>
-        <v-col>
-          <v-text-field
-            v-model="search_term"
-            label="Issue text"
-            clearable
-            dense
-            prepend-icon="short_text"
-            @input="$emit('search_term', search_term)"
-          />
-        </v-col>
-        <v-col>
-          <v-select
-            v-model="closed_status"
-            :items="['Yes', 'No']"
-            label="Resolved"
-            prepend-icon="fa-check"
-            clearable
-            dense
-            @input="$emit('closed_status', closed_status)"
-          />
-        </v-col>
-        <v-col>
-          <v-select
-            v-model="resolution"
-            :items="['Resolved', 'False Positive', 'Accepted Risk']"
-            label="Resolution Status"
-            prepend-icon="thumbs_up_down"
-            clearable
-            dense
-            @input="$emit('resolution', resolution)"
-          />
-        </v-col>
-        <v-col>
-          <v-select
-            v-model="ticket_status"
-            :items="['Yes', 'No']"
-            label="Ticket"
-            prepend-icon="mdi-cards"
-            clearable
-            dense
-            @input="$emit('ticket_status', ticket_status)"
-          />
-        </v-col>
-        <v-col>
-          <v-select
-            v-model="timeback"
-            :items="dates"
-            item-text="text"
-            item-value="value"
-            label="Date"
-            prepend-icon="mdi-clock-outline"
-            clearable
-            dense
-            @input="$emit('timeback', timeback)"
-          />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-select
-            v-model="risk_level"
-            :items="severities"
-            prepend-icon="fa-bug"
-            label="Risk"
-            clearable
-            multiple
-            chips
-            dense
-            @input="$emit('risk_level', risk_level)"
-          >
-            <template v-slot:selection="data">
-              <v-chip
-                :input-value="data.selected"
-                close
-                class="chip--select-multi"
-                @click:close="remove(risk_level, data.item)"
-              >
-                {{ data.item }}
-              </v-chip>
-            </template>
-          </v-select>
-        </v-col>
-        <v-col>
-          <v-autocomplete
-            v-model="templates"
-            :items="templatesNames"
-            label="Template"
-            chips
-            dense
-            multiple
-            clearable
-            prepend-icon="mdi-file"
-            @input="$emit('templates', templates)"
-          >
-            <template v-slot:selection="data">
-              <v-chip
-                :input-value="data.selected"
-                close
-                class="chip--select-multi"
-                @click:close="remove(templates, data.item)"
-              >
-                {{ data.item }}
-              </v-chip>
-            </template>
-          </v-autocomplete>
-        </v-col>
-        <v-col>
-          <v-autocomplete
-            v-model="tags"
-            :items="templatesTags"
-            label="Tags"
-            chips
-            dense
-            multiple
-            clearable
-            prepend-icon="mdi-tag-multiple"
-            @input="$emit('tags', tags)"
-          >
-            <template v-slot:selection="data">
-              <v-chip
-                :input-value="data.selected"
-                close
-                class="chip--select-multi"
-                @click:close="remove(data.item)"
-              >
-                {{ data.item }}
-              </v-chip>
-            </template>
-          </v-autocomplete>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+  <v-expansion-panels popout>
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <template v-slot:default>
+          <v-row no-gutters>
+            <v-col>
+              <v-fade-transition leave-absolute>
+                <span
+                  v-if="appliedFilters.length === 0"
+                  key="0"
+                  class="text--secondary"
+                >
+                  <i>Apply filters</i>
+                </span>
+                <span v-else key="1">
+                  <v-row no-gutters justify="start">
+                    <v-chip
+                      v-for="(item, index) in appliedFilters"
+                      :key="index"
+                      class="mx-2"
+                      label
+                      close
+                      @click:close="clearSelection(item)"
+                    >
+                      <v-icon
+                        color="primary"
+                        left
+                        small
+                      >{{ getIcon(item.name) }}</v-icon>
+                      <strong class="text-capitalize">{{ item.value }}</strong>
+                    </v-chip>
+                  </v-row>
+                </span>
+              </v-fade-transition>
+            </v-col>
+          </v-row>
+        </template>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-card outlined>
+          <v-container class="mb-1">
+            <v-row justify="start" align="start">
+              <v-col cols="2">
+                <strong class="text--secondary pl-1">Text search</strong>
+                <v-autocomplete
+                  id="search"
+                  ref="search"
+                  v-model="search_term"
+                  class="pt-2"
+                  :items="keywords"
+                  :search-input.sync="search"
+                  outlined
+                  dense
+                  hide-no-data
+                  clearable
+                  @keydown.esc="onEsc"
+                >
+                  <template slot="label">
+                    <v-icon style="vertical-align: middle">
+                      search
+                    </v-icon>Press &quot;/&quot; to focus
+                  </template>
+                </v-autocomplete>
+              </v-col>
+              <v-divider class="mx-3" vertical />
+              <filter-option
+                :items="statusFilterItems"
+                name="Status"
+                :selection.sync="selectedStatuses"
+              />
+              <v-divider class="mx-3" vertical />
+              <filter-option
+                :items="riskFilterItems"
+                name="Risk"
+                :selection.sync="selectedRisks"
+              />
+              <v-divider class="mx-3" vertical />
+              <filter-option
+                :items="templateFilterItems"
+                name="Template"
+                :selection.sync="selectedTemplates"
+              />
+              <v-divider class="mx-3" vertical />
+              <filter-option
+                :items="resolutionFilterItems"
+                name="Resolution"
+                :selection.sync="selectedResolutions"
+              />
+              <v-divider class="mx-3" vertical />
+              <filter-option
+                :items="ticketFilterItems"
+                name="Ticket"
+                :selection.sync="selectedTicketStatus"
+              />
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 <script>
 import { mapGetters } from 'vuex';
 import { TEMPLATES_FETCH } from '@/store/actions';
+import FilterOption from '@/components/FilterOption.vue';
+import { capitalize } from 'lodash';
 
 export default {
   name: 'IssueFilter',
+  components: { FilterOption },
+  props: {
+    keywords: {
+      type: Array,
+      required: true,
+    },
+    riskFilterItems: {
+      type: Array,
+      required: true,
+    },
+    templateFilterItems: {
+      type: Array,
+      required: true,
+    },
+    statusFilterItems: {
+      type: Array,
+      required: true,
+    },
+    resolutionFilterItems: {
+      type: Array,
+      required: true,
+    },
+    ticketFilterItems: {
+      type: Array,
+      required: true,
+    },
+  },
   data() {
     return {
-      resolution: '',
       search_term: '',
-      ticket_status: '',
-      closed_status: 'No',
-      startDate: '',
-      endDate: '',
-      risk_level: [],
-      model: null,
-      menu2: false,
-      menu3: false,
-      templates: [],
-      tags: [],
+      search: null,
       timeback: '',
+      selectedRisks: [],
+      selectedTemplates: [],
+      selectedStatuses: ['open'],
+      selectedResolutions: [],
+      selectedTicketStatus: [],
       dates: [
         {
           text: 'last day',
@@ -171,19 +159,141 @@ export default {
           value: '7',
         },
       ],
-      severities: ['info', 'low', 'medium', 'high', 'critical'],
     };
   },
+
   computed: {
     ...mapGetters(['templatesNames', 'templatesTags']),
+
+    appliedFilters() {
+      const result = [];
+
+      if (this.search) {
+        result.push({ name: 'search', icon: 'short_text', value: this.search });
+      }
+
+      if (this.selectedStatuses.length > 0) {
+        this.selectedStatuses.forEach((s) =>
+          result.push({
+            name: 'status',
+            value: s,
+          })
+        );
+      }
+
+      if (this.selectedTemplates.length > 0) {
+        this.selectedTemplates.forEach((t) =>
+          result.push({
+            name: 'template',
+            value: t,
+          })
+        );
+      }
+
+      if (this.selectedTicketStatus.length > 0) {
+        this.selectedTicketStatus.forEach((t) =>
+          result.push({
+            name: 'ticket',
+            value: t,
+          })
+        );
+      }
+
+      if (this.selectedResolutions.length > 0) {
+        this.selectedResolutions.forEach((r) =>
+          result.push({
+            name: 'resolution',
+            value: r,
+          })
+        );
+      }
+
+      if (this.selectedRisks.length > 0) {
+        this.selectedRisks.forEach((r) =>
+          result.push({
+            name: 'risk',
+            value: r,
+          })
+        );
+      }
+
+      return result;
+    },
   },
+
+  watch: {
+    // eslint-disable-next-line no-unused-vars
+    appliedFilters(newValue, oldValue) {
+      this.$emit('filter_update', newValue);
+    },
+  },
+
   mounted() {
     this.$store.dispatch(TEMPLATES_FETCH);
+
+    document.onkeydown = (e) => {
+      // eslint-disable-next-line no-param-reassign
+      e = e || window.event;
+      if (
+        e.keyCode === 191 && // Forward Slash '/'
+        e.target !== this.$refs.search.$refs.input
+      ) {
+        e.preventDefault();
+        this.$refs.search.focus();
+      }
+    };
   },
+
   methods: {
-    remove(array, item) {
-      const index = array.indexOf(item);
-      if (index >= 0) array.splice(index, 1);
+    onEsc() {
+      this.$refs.search.blur();
+    },
+
+    getIcon(name) {
+      switch (name) {
+        case 'risk':
+          return 'fa-bug';
+          break;
+        case 'resolution':
+          return 'mdi-thumbs-up-down';
+          break;
+        case 'ticket':
+          return 'mdi-cards';
+          break;
+        case 'status':
+          return 'mdi-checkbox-marked-circle-outline';
+          break;
+        case 'template':
+          return 'mdi-file';
+          break;
+        case 'search':
+          return 'short_text';
+          break;
+      }
+    },
+
+    clearSelection(item) {
+      switch (item.name) {
+        case 'risk':
+          this.selectedRisks = this.selectedRisks.filter((r) => r !== item.value);
+          break;
+        case 'template':
+          this.selectedTemplates = this.selectedTemplates.filter((t) => t !== item.value);
+          break;
+        case 'status':
+          this.selectedStatuses = this.selectedStatuses.filter((s) => s !== item.value);
+          break;
+        case 'resolution':
+          this.selectedResolutions = this.selectedResolutions.filter((r) => r !== item.value);
+          break;
+        case 'ticket':
+          this.selectedTicketStatus = this.selectedTicketStatus.filter((s) => s !== item.value);
+          break;
+        case 'search':
+          this.search_term = '';
+          this.search = '';
+          break;
+      }
     },
   },
 };
