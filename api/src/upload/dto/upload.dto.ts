12a15d01412b2947b7d60d9f4d4afde45cd6fb17
faloker <faloker@tuta.io>
2020-07-06 12:38:52 +0300

    Add new controller /upload

---
+++ api/src/upload/dto/upload.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty } from "class-validator";
+
+export class UnitAndTemplateSlugsDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly unitSlug: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly templateSlug: string;
+}
+
+export class UnitSlugDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  readonly unitSlug: string;
+}
+
+export class FileUploadDto {
+  @ApiProperty({ type: 'string', format: 'binary' })
+  file: any;
+}
