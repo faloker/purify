5087fa7c6d83e9ee6efa2ddd46e17e44c33e27ce
faloker <faloker@tuta.io>
2020-07-19 20:45:11 +0300

    Send as string

--- web/src/components/dialogs/EditIssueDialog.vue
+++ web/src/components/dialogs/EditIssueDialog.vue
@@ -44,7 +44,7 @@ export default {
   },
   methods: {
     updateIssue() {
-      const change = { fields: JSON.parse(this.rawIssue) };
+      const change = { fields: this.rawIssue };
 
       this.$store.dispatch(ISSUE_UPDATE, { ids: [this.issue._id], change }).then(() => {
         this.issue.fields = JSON.parse(change.fields);
